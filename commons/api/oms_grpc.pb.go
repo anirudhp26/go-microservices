279bef45b7c848322dc58db7382e76d08013606d
anirudhp26 <anirudh26p@gmail.com>
2024-12-25 22:33:06 +0530

    FIX: issues fix and progress in gRPC

--- commons/api/oms_grpc.pb.go
+++ commons/api/oms_grpc.pb.go
@@ -263,3 +263,147 @@ var PaymentService_ServiceDesc = grpc.ServiceDesc{
 	Streams:  []grpc.StreamDesc{},
 	Metadata: "api/oms.proto",
 }
+
+const (
+	StockService_CheckOutStock_FullMethodName = "/api.StockService/CheckOutStock"
+	StockService_CheckInStock_FullMethodName  = "/api.StockService/CheckInStock"
+)
+
+// StockServiceClient is the client API for StockService service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
+//
+// Stock service
+type StockServiceClient interface {
+	CheckOutStock(ctx context.Context, in *CheckOutStockRequest, opts ...grpc.CallOption) (*CheckOutStockResponse, error)
+	CheckInStock(ctx context.Context, in *CheckInStockRequest, opts ...grpc.CallOption) (*CheckInStockResponse, error)
+}
+
+type stockServiceClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewStockServiceClient(cc grpc.ClientConnInterface) StockServiceClient {
+	return &stockServiceClient{cc}
+}
+
+func (c *stockServiceClient) CheckOutStock(ctx context.Context, in *CheckOutStockRequest, opts ...grpc.CallOption) (*CheckOutStockResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(CheckOutStockResponse)
+	err := c.cc.Invoke(ctx, StockService_CheckOutStock_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *stockServiceClient) CheckInStock(ctx context.Context, in *CheckInStockRequest, opts ...grpc.CallOption) (*CheckInStockResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(CheckInStockResponse)
+	err := c.cc.Invoke(ctx, StockService_CheckInStock_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// StockServiceServer is the server API for StockService service.
+// All implementations must embed UnimplementedStockServiceServer
+// for forward compatibility.
+//
+// Stock service
+type StockServiceServer interface {
+	CheckOutStock(context.Context, *CheckOutStockRequest) (*CheckOutStockResponse, error)
+	CheckInStock(context.Context, *CheckInStockRequest) (*CheckInStockResponse, error)
+	mustEmbedUnimplementedStockServiceServer()
+}
+
+// UnimplementedStockServiceServer must be embedded to have
+// forward compatible implementations.
+//
+// NOTE: this should be embedded by value instead of pointer to avoid a nil
+// pointer dereference when methods are called.
+type UnimplementedStockServiceServer struct{}
+
+func (UnimplementedStockServiceServer) CheckOutStock(context.Context, *CheckOutStockRequest) (*CheckOutStockResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CheckOutStock not implemented")
+}
+func (UnimplementedStockServiceServer) CheckInStock(context.Context, *CheckInStockRequest) (*CheckInStockResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CheckInStock not implemented")
+}
+func (UnimplementedStockServiceServer) mustEmbedUnimplementedStockServiceServer() {}
+func (UnimplementedStockServiceServer) testEmbeddedByValue()                      {}
+
+// UnsafeStockServiceServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to StockServiceServer will
+// result in compilation errors.
+type UnsafeStockServiceServer interface {
+	mustEmbedUnimplementedStockServiceServer()
+}
+
+func RegisterStockServiceServer(s grpc.ServiceRegistrar, srv StockServiceServer) {
+	// If the following call pancis, it indicates UnimplementedStockServiceServer was
+	// embedded by pointer and is nil.  This will cause panics if an
+	// unimplemented method is ever invoked, so we test this at initialization
+	// time to prevent it from happening at runtime later due to I/O.
+	if t, ok := srv.(interface{ testEmbeddedByValue() }); ok {
+		t.testEmbeddedByValue()
+	}
+	s.RegisterService(&StockService_ServiceDesc, srv)
+}
+
+func _StockService_CheckOutStock_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(CheckOutStockRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(StockServiceServer).CheckOutStock(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: StockService_CheckOutStock_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(StockServiceServer).CheckOutStock(ctx, req.(*CheckOutStockRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _StockService_CheckInStock_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(CheckInStockRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(StockServiceServer).CheckInStock(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: StockService_CheckInStock_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(StockServiceServer).CheckInStock(ctx, req.(*CheckInStockRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+// StockService_ServiceDesc is the grpc.ServiceDesc for StockService service.
+// It's only intended for direct use with grpc.RegisterService,
+// and not to be introspected or modified (even as a copy)
+var StockService_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "api.StockService",
+	HandlerType: (*StockServiceServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "CheckOutStock",
+			Handler:    _StockService_CheckOutStock_Handler,
+		},
+		{
+			MethodName: "CheckInStock",
+			Handler:    _StockService_CheckInStock_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "api/oms.proto",
+}
